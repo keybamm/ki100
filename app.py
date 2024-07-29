9dbf96fc1c242daabf1d79b01010163e8d9e9388
keybamm <alnnm0922@gmail.com>
2024-07-29 22:06:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title('동물 이미지 찾아주기🙂')
-name = st.text_input('영어로 동물을 입력하세요')
+name = st.text_input('한국어로 동물을 입력하세요')
 
 if st.button('찾아보기'):
     st.image(f'https://edu.spartacodingclub.kr/random/?{name}')
